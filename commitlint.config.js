976abcd5d3a7844e0f7fd47a8092fc1ef4571e72
Dennislee:) <55692615+dennislee928@users.noreply.github.com>
2026-06-05 22:11:02 +0800

    Auto-commit: Project implementation in progress [count: 204]

---
+++ commitlint.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', 'fix', 'docs', 'style', 'refactor',
+        'perf', 'test', 'build', 'ci', 'chore', 'revert',
+        'security', 'quantum', 'zk', 'wasm', 'fl', 'apq',
+      ],
+    ],
+    'subject-max-length': [2, 'always', 100],
+    'body-max-line-length': [2, 'always', 200],
+  },
+};
